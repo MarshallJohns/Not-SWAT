e162c488dcd88a9b98cee28ef3a15bbe433747e0
MarshallJohns <marshalljohnson193@gmail.com>
2025-05-24 12:49:57 -0600

    moved character to its own scene

--- Scripts/character_movement.gd
+++ Scripts/character_movement.gd
@@ -18,11 +18,21 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var right_x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+	var right_y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	var shoot_dir = Vector2(right_x, right_y)
+	if shoot_dir.length() > 0.2:
+		shoot_dir = shoot_dir.normalized()
+		print(shoot_dir)
+	# Use shoot_dir for aiming/shooting
+
 
 	move_and_slide()
